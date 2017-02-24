35d2c34dfda221589d327d4255bd9c4a7ca49e54
priyanka <priyankaprakash5794@gmail.com>
2017-02-24 11:55:01 +0530

    Add db configurations

--- config/utils.js
+++ config/utils.js
@@ -1,15 +1,18 @@
 const pg = require('pg')
+let confFile = __dirname + '/db_settings.json'
+const setting = require(confFile)
+const dbconfig = setting.dbconfig
 
-const utils = {}
-
-let conString = 'pg://postgres:qwerty@localhost:5432/test'
+let conString = 'pg://' + dbconfig.username + ':'+ dbconfig.password + '@' + dbconfig.dbhost + ':' + dbconfig.dbport + '/' + dbconfig.dbname
 let client = new pg.Client(conString)
 client.connect()
+//let query = client.query('SELECT 1 from pg_database WHERE datname=test')
+console.log(query)
+client.query('CREATE TABLE IF NOT EXISTS courses(courseId serial PRIMARY KEY,coursename varchar(35), startdate varchar(20), enddate varchar(20))')
+const utils = {}
 
 utils.createCourse = (req, res) => {
-  client.query('CREATE TABLE IF NOT EXISTS courses(courseId serial PRIMARY KEY,coursename varchar(35), startdate varchar(12), enddate varchar(12))')
-  console.log(typeof req.body.name)
-  if ((typeof req.body.name === 'string') && (req.body.start !== null) && (req.body.end !== null)) {
+  if ((req.body.name.trim().length > 0)) {
     let query = client.query("INSERT INTO courses (coursename, startdate, enddate) VALUES ('"+req.body.name+"', '"+req.body.start+"', '"+req.body.end+"')")
     res.send(req.body)
   } else {
