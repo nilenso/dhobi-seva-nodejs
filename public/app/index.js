720a3e9f3ec2b5db12e005d4fb4d48727a41d5c6
Garima Kamboj <garima.kamboj@icloud.com>
2017-03-02 23:45:35 +0530

    React Set Up

---
+++ public/app/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Card from './components/Card';
+
+
+ReactDOM
+.render(<div>Hello World</div>,
+  document.getElementById('app'))
