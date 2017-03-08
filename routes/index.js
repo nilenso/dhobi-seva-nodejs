014b049ce4375a4407ce8ca7efb50aad0cd334c3
Garima Kamboj <garima.kamboj@icloud.com>
2017-03-08 10:28:04 +0530

    Fix endCourse routes

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,13 @@ const express = require('express')
 const courseRoutes = require('./courseRoutes')
 const studentRoutes = require('./studentRoutes')
 const transactionRoutes = require('./transactionRoutes')
+const endCourseRoutes = require('./endCourseRoutes')
 
 const router = express.Router()
 
 courseRoutes(router)
 studentRoutes(router)
 transactionRoutes(router)
+endCourseRoutes(router)
 
 module.exports = router
