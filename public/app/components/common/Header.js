f6aa7042916ba5ce1e91776407c7972b8dd8ec74
Garima Kamboj <garima.kamboj@icloud.com>
2017-03-09 15:32:14 +0530

    Add 'Courses' button to navbar

--- public/app/components/common/Header.js
+++ public/app/components/common/Header.js
@@ -1,5 +1,7 @@
 import React from 'react'
+import { Link } from 'react-router'
 import 'materialize-css/bin/materialize.css'
+import 'main.css'
 
 const Header = (props) => {
   return (
@@ -7,7 +9,12 @@ const Header = (props) => {
       <div className="navbar-fixed">
         <nav>
           <div className="nav-wrapper">
-            <div className="brand-logo center">{props.title}</div>
+            <div className="brand-logo page-title">{props.title}</div>
+            <ul id="nav-mobile" className="right hide-on-med-and-down">
+              <li>
+                <Link to={`/courses`} className="nav-button">Courses</Link>
+              </li>
+            </ul>
           </div>
         </nav>
       </div>
