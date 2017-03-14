b72a56f1f0828797949bcbc8ed828f19bac29f4c
Garima Kamboj <garima.kamboj@icloud.com>
2017-03-14 13:40:14 +0530

    Update if condition

--- controllers/endCourseController.js
+++ controllers/endCourseController.js
@@ -4,7 +4,7 @@ exports.endCourse = (req, res) => {
   let courseId = req.params.courseId
   db.endCourse(courseId,
     function (listOfStudents) {
-      if (listOfStudents.length > 0) res.send(listOfStudents)
+      if (listOfStudents.length >= 0) res.send(listOfStudents)
       else res.status(500).send('ERROR')
     })
 }
