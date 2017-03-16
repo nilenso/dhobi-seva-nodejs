c07ce88740a0f55b61a019ed6612ac0115f9c0e1
priyanka <priyankaprakash5794@gmail.com>
2017-03-16 11:54:30 +0530

    Add validation tests for  - deposit  - expense  - user

--- tests/validate.test.js
+++ tests/validate.test.js
@@ -12,3 +12,36 @@ test('valid student input', () => {
     seat_number: '2'
   })
 })
+
+test('valid deposit input', () => {
+  let deposit = {
+    amount: 500
+  }
+  expect(validate.depositDetails(deposit)).toEqual({
+    amount: 500
+  })
+})
+
+test('valid expense input', () => {
+  let expense = {
+    transaction_name: 'laundry',
+    quantity: 2,
+    rate: 20
+  }
+  expect(validate.expenseDetails(expense)).toEqual({
+    transaction_name: 'laundry',
+    quantity: 2,
+    rate: 20
+  })
+})
+
+test('valid user input', () => {
+  let user = {
+    user_name: 'Priya',
+    user_id: 8241643434
+  }
+  expect(validate.userDetails(user)).toEqual({
+    user_name: 'Priya',
+    user_id: 8241643434
+  })
+})
